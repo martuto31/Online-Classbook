966ede6261d8bc486aa779b52d863a600229860b
Martin Petrov <martuto31@gmail.com>
2022-06-21 18:28:33 +0300

    Added TeacherAccountService and interface with CheckIfTeacherAccountExists function.

---
+++ School_Register.Services/Account/ITeacherAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School_Register.Services.Account
+{
+    public interface ITeacherAccountService
+    {
+        public bool CheckIfAccountExists(string username);
+    }
+}
---
+++ School_Register.Services/Account/TeacherAccountService.cs
@@ -0,0 +1,25 @@
+﻿using School_Register.Data.Models;
+using School_Register.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace School_Register.Services.Account
+{
+    public class TeacherAccountService : ITeacherAccountService
+    {
+        private readonly IRepository<TeacherAccount> teacherAccRepo;
+
+        public TeacherAccountService(IRepository<TeacherAccount> teacherAccRepo)
+        {
+            this.teacherAccRepo = teacherAccRepo;
+        }
+
+        public bool CheckIfAccountExists(string username)
+        {
+            return this.teacherAccRepo.All()
+                .Any(a => a.Username == username);
+        }
+    }
+}
